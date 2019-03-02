da8f3af4eb66defca121156cb6b01e236917e9f3
Martin Drozdík <drozdik.m@seznam.cz>
2019-03-02 13:50:10 +0100

    Add backgroud music controller comments

--- Unity/Assets/Scripts/Audio/BackgroundMusic.cs
+++ Unity/Assets/Scripts/Audio/BackgroundMusic.cs
@@ -5,9 +5,24 @@ using UnityEngine;
 public class BackgroundMusic : MonoBehaviour
 {
     //Theme music files
+    /// <summary>
+    /// Spooky music clip
+    /// </summary>
     public AudioClip SpookyMusic;
+
+    /// <summary>
+    /// Scary music clip
+    /// </summary>
     public AudioClip ScaryMusic;
+
+    /// <summary>
+    /// Regular music clip
+    /// </summary>
     public AudioClip RegularMusic;
+
+    /// <summary>
+    /// Happy music clip
+    /// </summary>
     public AudioClip HappyMusic;
 
     //Players
@@ -20,6 +35,11 @@ public class BackgroundMusic : MonoBehaviour
         fadingController = GetComponent<FadingAudioSource>();
     }
 
+    /// <summary>
+    /// Change music background theme
+    /// </summary>
+    /// <param name="newTheme">New theme, music will transition (enum)</param>
+    /// <param name="transitionSpeed">Speed parameter (lower = slower), default is 0.3f</param>
     public void ChangeTheme(BackgroundMusicTheme newTheme, float transitionSpeed = 0.3f)
     {
         fadingController.FadeSpeed = transitionSpeed;
--- Unity/Assets/Scripts/Audio/tests/BackgroundMusic_testEnviroment.cs
+++ Unity/Assets/Scripts/Audio/tests/BackgroundMusic_testEnviroment.cs
@@ -4,16 +4,25 @@ using UnityEngine;
 
 public class BackgroundMusic_testEnviroment : MonoBehaviour
 {
+    /// <summary>
+    /// Testing method for spooky music
+    /// </summary>
     public void PlaySpookyMusic()
     {
         GameObject.FindGameObjectWithTag("BackgroundMusic").GetComponent<BackgroundMusic>().ChangeTheme(BackgroundMusicTheme.Spooky);
     }
 
+    /// <summary>
+    /// Testing method for no music
+    /// </summary>
     public void PlayNoMusic()
     {
         GameObject.FindGameObjectWithTag("BackgroundMusic").GetComponent<BackgroundMusic>().ChangeTheme(BackgroundMusicTheme.NoMusic);
     }
 
+    /// <summary>
+    /// Testing method for regular music
+    /// </summary>
     public void PlayRegularMusic()
     {
         GameObject.FindGameObjectWithTag("BackgroundMusic").GetComponent<BackgroundMusic>().ChangeTheme(BackgroundMusicTheme.Regular);
