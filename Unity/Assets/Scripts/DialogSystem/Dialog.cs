73f6a397f262da577238c387102547ddcc740962
Adam Gelatka <gelatada@fit.cvut.cz>
2019-03-04 19:50:11 +0100

    Add basic dialog system

---
+++ Unity/Assets/Scripts/DialogSystem/Dialog.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Dialog : MonoBehaviour
+{
+    public string characterName;
+
+    [TextArea(2,10)]
+    public string[] sentences;
+
+
+    public void triggerDialog()
+    {
+       DialogManager dm = FindObjectOfType<DialogManager>();
+       if (dm == null)
+        {
+            Debug.Log("Missing Dialog manager!");
+            return;
+        }
+
+        dm.startDialog(this);
+    }
+}
---
+++ Unity/Assets/Scripts/DialogSystem/DialogManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class DialogManager : MonoBehaviour
+{
+    public Text nameText;
+    public Text dialogText;
+
+    private string characterName;
+    private Queue<string> sentences;
+
+    void Start()
+    {
+        sentences = new Queue<string>();    
+    }
+
+    public void startDialog(Dialog dialog)
+    {
+        sentences.Clear();
+
+        foreach (var sentence in dialog.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+        characterName = dialog.characterName;
+
+        nextSentence();
+    }
+
+    public void nextSentence()
+    {
+        if (sentences.Count == 0)
+        {
+            endOfDialog();
+            return;
+        }
+
+        string sentenceToDisplay = sentences.Dequeue();
+
+        nameText.text = characterName;
+        dialogText.text = sentenceToDisplay;
+    }
+
+    void endOfDialog()
+    {
+        characterName = "";
+        Debug.Log("End of dialog!");
+    }
+}
