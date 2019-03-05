a5fe3dd81158b6f38f8613d1fa7782946d438cf9
Ian Mustiats <mustiian@fit.cvut.cz>
2019-03-05 16:27:39 +0100

    Add script for interactive button for testing

---
+++ Unity/Assets/Scripts/UI/TestScripts/InteractButton.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InteractButton : MonoBehaviour
+{
+    public GameObject InterectObject;
+    public Button button;
+
+    private void Start()
+    {
+        button.gameObject.SetActive (false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ( Vector3.Distance( InterectObject.transform.position, transform.position ) < 100)
+        {
+            Debug.Log ("Object is close enough");
+            //Transform canvas = transform.GetComponentInChildren<Transform> ();
+            button.gameObject.SetActive (true);
+        }
+    }
+}
