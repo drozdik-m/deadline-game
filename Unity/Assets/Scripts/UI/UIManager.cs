3ec639612a6726bcc3915f2636a766a727e37dd6
Ian Mustiats <mustiian@fit.cvut.cz>
2019-03-05 13:58:07 +0100

    Add pause Menu for pausing game

---
+++ Unity/Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// User Interface manager
+/// </summary>
+public class UIManager : MonoBehaviour
+{
+    /// <summary>
+    /// Menu panel
+    /// </summary>
+    public GameObject MenuPanel;
+    private bool menuOpen;
+
+    private void Start()
+    {
+        MenuPanel.SetActive (false);
+        menuOpen = false;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown (KeyCode.Escape))
+        {
+            ChangeMenuStatus ();
+        }
+    }
+
+    /// <summary>
+    /// Open or close Menu. If Menu is open, stop background scene.
+    /// </summary>
+    public void ChangeMenuStatus()
+    {
+        // Change value to opposite
+        menuOpen = !menuOpen;
+
+        if (menuOpen)
+            Time.timeScale = 0f;
+        else
+           Time.timeScale = 1f;
+
+        MenuPanel.SetActive (menuOpen);
+    }
+}
