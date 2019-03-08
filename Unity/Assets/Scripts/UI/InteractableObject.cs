70fac4565236d6c5537fe9c4c39cfb3e31f9f21c
Ian Mustiats <mustiian@fit.cvut.cz>
2019-03-08 20:32:21 +0100

    Add script for Interactable Button. Add Animation for this button.

---
+++ Unity/Assets/Scripts/UI/InteractableObject.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// Class for interactable items. 
+/// If the player close to the item, the interactable button wil be appear.
+/// </summary>
+public class InteractableObject : MonoBehaviour
+{
+    public GameObject Player;
+    /// <summary>
+    /// Button, that will be appear
+    /// </summary>
+    public Button InteractableButton;
+    /// <summary>
+    /// Minimum distance between item and player, when button will be appear.
+    /// </summary>
+    public float distance = 100f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InteractableButton.gameObject.SetActive (false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance (Player.transform.position, transform.position) < distance)
+        {
+            Transform canvas = transform.GetComponentInChildren<Transform> ();
+            InteractableButton.gameObject.SetActive (true);
+        }
+        else
+        {
+            InteractableButton.gameObject.SetActive (false);
+        }
+    }
+}
