6549800246566cce556595a9a780672c1478cef6
Adam Gelatka <gelatada@fit.cvut.cz>
2019-03-12 17:08:39 +0100

    Add bubble graphics

---
+++ Unity/Assets/Scripts/DialogSystem/TwinTalkDialog.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Twin-talk Dialog", menuName = "Dialog/Twin-talk dialog")]
+public class TwinTalkDialog : ScriptableObject
+{
+    [System.Serializable]
+    public class SentenceStructure
+    {
+        public int characterId;
+        [TextArea(2, 30)]
+        public string sentence;
+    }
+
+    public SentenceStructure[] structuredSentences;
+
+
+}
