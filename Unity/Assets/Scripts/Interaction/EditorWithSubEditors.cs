95e72f8cd4083f51c7cba67cf8492a83a5fd2bc6
Lukáš Pekař <lukaspekar8@gmail.com>
2019-03-12 22:05:41 +0100

    add conditions logic including editors

---
+++ Unity/Assets/Scripts/Interaction/EditorWithSubEditors.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEditor;
+using UnityEngine;
+
+public abstract class EditorWithSubEditors<TEditor, TTarget> : Editor
+    where TEditor : Editor
+    where TTarget : UnityEngine.Object
+{
+    protected TEditor[] subEditors;
+
+    protected void CheckAndCreateSubEditors(TTarget[] subEditorTargets)
+    {
+        if (subEditors != null && subEditors.Length == subEditorTargets.Length)
+            return;
+
+        CleanupEditors();
+
+        subEditors = new TEditor[subEditorTargets.Length];
+
+        for (int i = 0; i < subEditors.Length; i++)
+        {
+            subEditors[i] = CreateEditor(subEditorTargets[i]) as TEditor;
+            SubEditorSetup(subEditors[i]);
+        }
+    }
+
+    protected void CleanupEditors()
+    {
+        if (subEditors == null) return;
+
+        for (int i = 0; i < subEditors.Length; i++)
+            DestroyImmediate(subEditors[i]);
+
+        subEditors = null;
+    }
+
+    protected abstract void SubEditorSetup(TEditor editor);
+}
---
+++ Unity/Assets/Scripts/Interaction/ReactionCollection.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReactionCollection : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        throw new NotImplementedException();
+    }
+
+    // Update is called once per frame
+    public void React()
+    {
+        throw new NotImplementedException();
+    }
+}
