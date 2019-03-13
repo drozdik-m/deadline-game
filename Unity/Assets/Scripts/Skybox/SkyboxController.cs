2f7ed13bbc90f7f871af00574da37f6a7408fc8c
Ian Mustiats <mustiian@fit.cvut.cz>
2019-03-13 11:00:34 +0100

    Add cinemachine camera and player to scenes. Update script for better working with camera

--- Unity/Assets/Scripts/Skybox/SkyboxController.cs
+++ Unity/Assets/Scripts/Skybox/SkyboxController.cs
@@ -42,6 +42,7 @@ public class SkyboxController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        MainCamera.clearFlags = CameraClearFlags.SolidColor;
         ChangeTime ();
     }
 
