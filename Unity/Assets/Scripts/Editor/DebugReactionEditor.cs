f9de875060b4de5364d1da37d3959226f1c3db69
Lukáš Pekař <lukaspekar8@gmail.com>
2019-03-28 12:30:33 +0100

    add interactable object add interactable editor

---
+++ Unity/Assets/Scripts/Editor/DebugReactionEditor.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using Assets.Scripts.Interaction.Reactions.ImmediateReactions;
+
+namespace Assets.Scripts.Interaction.Reactions.Editor
+{
+    [CustomEditor(typeof(DebugReaction))]
+    public class DebugReactionEditor : ReactionEditor
+    {
+        private SerializedProperty debugMessageProperty;
+
+        private const string debugReactionPropDebugMessageName = "debugMessage";
+        private const float areaWidthOfSet = 19f;
+        private const float debugMessageGUILines = 3f;
+
+        protected override void Init()
+        {
+            debugMessageProperty = serializedObject.FindProperty(debugReactionPropDebugMessageName);
+        }
+
+        protected override void DrawReaction()
+        {
+            EditorGUILayout.BeginHorizontal();
+
+            EditorGUILayout.LabelField("Message", GUILayout.Width(EditorGUIUtility.labelWidth - areaWidthOfSet)); ;
+            debugMessageProperty.stringValue =
+                EditorGUILayout.TextArea(debugMessageProperty.stringValue,
+                    GUILayout.Height(EditorGUIUtility.singleLineHeight * debugMessageGUILines));
+
+            EditorGUILayout.EndHorizontal();
+
+            // easy way to do property fields
+            //EditorGUILayout.PropertyField(debugMessageProperty);
+        }
+
+        protected override string GetFoldoutLabel()
+        {
+            return "Debug Reaction";
+        }
+    }
+}
---
+++ Unity/Assets/Scripts/Interaction/Interactable.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.Interaction
+{
+    public class Interactable : MonoBehaviour
+    {
+        public Transform interactionLocation;
+        public ConditionCollection[] conditionCollections =
+            new ConditionCollection[0];
+        public ReactionCollection defaultReactionCollection;
+
+        public void Interact()
+        {
+            for (int i = 0; i < conditionCollections.Length; i++)
+            {
+                // if one of the collections react, we don't want
+                // the default reaction to be done
+                if (conditionCollections[i].CheckAndReact())
+                    return;
+            }
+
+            defaultReactionCollection.React();
+        }
+
+    }
+}
