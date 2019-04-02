389da3f128b026b0ca20736b7829a9f31fafee49
Martin Drozdík <drozdik.m@seznam.cz>
2019-04-02 19:02:57 +0200

    Add quest stages

---
+++ Unity/Assets/Scripts/StageManager/StageTypes/StageQuest.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageQuest : Stage
+{
+    public Quest quest;
+
+    public override bool ReadyForNextStage()
+    {
+        return quest.IsCompleted();
+    }
+
+    public override void StageEnd()
+    {
+        
+    }
+
+    public override void StageFixedUpdate()
+    {
+        
+    }
+
+    public override void StageLoad()
+    {
+        
+    }
+
+    public override void StageUpdate()
+    {
+        
+    }
+}
