8e5faea61676c16daab0abccdec94bc03492d2f4
Ian Mustiats <mustiian@fit.cvut.cz>
2019-04-03 20:38:03 +0200

    Add new images for UI Inventory and some test images, update scene

--- Unity/Assets/Scripts/InventoryUI/InventoryUIController.cs
+++ Unity/Assets/Scripts/InventoryUI/InventoryUIController.cs
@@ -1,22 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class InventoryUIController : MonoBehaviour
 {
 
-    public Inventory inventory;
+    public Inventory Inventory;
+
+    public Sprite[] ItemsSpritesStore;
+
+    public Image ActualImageItem;
 
     private bool isInventoryEmpty;
 
     // Start is called before the first frame update
     void Start()
     {
-        inventory.OnChange += OnChange;
+        Inventory.OnChange += OnChange;
+        isInventoryEmpty = true;
     }
 
-    public void OnChange( Inventory caller, InventoryChangeEventArgs e)
+    public void OnChange(Inventory caller, InventoryChangeEventArgs e)
     {
         Debug.Log ("On change " + e.newItemType.ToString() );
+    }
+
+    private void chooseSprite(InventoryItemID item)
+    {
+        if (item == InventoryItemID.Food)
+            ActualImageItem.sprite = ItemsSpritesStore[0];
+        else if (item == InventoryItemID.Gun)
+            ActualImageItem.sprite = ItemsSpritesStore[1];
     }
 }
