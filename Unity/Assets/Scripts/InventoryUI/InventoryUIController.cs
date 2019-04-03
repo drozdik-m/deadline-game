b0cc18d3e933b663a50a64c883a5c77cf0a690d3
Ian Mustiats <mustiian@fit.cvut.cz>
2019-04-03 20:51:52 +0200

    Add methods for drop and pick up items, save scene.

--- Unity/Assets/Scripts/InventoryUI/InventoryUIController.cs
+++ Unity/Assets/Scripts/InventoryUI/InventoryUIController.cs
@@ -2,14 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+
 public class InventoryUIController : MonoBehaviour
 {
 
     public Inventory Inventory;
 
-    public Sprite[] ItemsSpritesStore;
-
     public Image ActualImageItem;
+
+    public Text ItemsDescription;
+
+    public Sprite[] SpritesStore;
 
     private bool isInventoryEmpty;
 
@@ -17,19 +20,45 @@ public class InventoryUIController : MonoBehaviour
     void Start()
     {
         Inventory.OnChange += OnChange;
+        ActualImageItem.sprite = null;
+        ItemsDescription.text = "";
         isInventoryEmpty = true;
     }
 
-    public void OnChange(Inventory caller, InventoryChangeEventArgs e)
+    public void OnChange(Inventory caller, InventoryChangeEventArgs item)
     {
-        Debug.Log ("On change " + e.newItemType.ToString() );
+        if (isInventoryEmpty)
+        {
+            PickUp (item);
+            isInventoryEmpty = false;
+        } else
+        {
+            Drop (item);
+            isInventoryEmpty = true;
+        }
+
     }
 
-    private void chooseSprite(InventoryItemID item)
+    private Sprite chooseSprite(InventoryItemID item)
     {
         if (item == InventoryItemID.Food)
-            ActualImageItem.sprite = ItemsSpritesStore[0];
+            return SpritesStore[0];
         else if (item == InventoryItemID.Gun)
-            ActualImageItem.sprite = ItemsSpritesStore[1];
+            return SpritesStore[1];
+
+        return null;
+    }
+
+    private void PickUp(InventoryChangeEventArgs item)
+    {
+        ActualImageItem.sprite = chooseSprite (item.newItemType);
+        ItemsDescription.text = item.newItemType.ToString ();
+
+    }
+
+    private void Drop(InventoryChangeEventArgs item)
+    {
+        ActualImageItem.sprite = null;
+        ItemsDescription.text = "";
     }
 }
