e8604daf28e304b03015fa0e704705247dce4dd1
Ian Mustiats <mustiian@fit.cvut.cz>
2019-04-07 14:41:59 +0200

    Add function that change quest UI

--- Unity/Assets/Scripts/QuestsUI/QuestsUIController.cs
+++ Unity/Assets/Scripts/QuestsUI/QuestsUIController.cs
@@ -1,25 +1,48 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class QuestsUIController : MonoBehaviour
 {
     public QuestStack QuestStorage;
 
+    [System.Serializable]
+    public struct QuestStatus
+    {
+        public Text QuestDescriptionText;
+        public Image QuestStatusImage;
+    }
+
+    public QuestStatus[] QuestsUI;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (QuestStorage != null)
+            QuestStorage.OnChange += ChangeQuestsUI;
     }
 
-    // Update is called once per frame
-    void Update()
+    void ChangeQuestsUI(QuestStack questStack, QuestStackArgs args)
     {
-        
+        Debug.Log ("Change UI quests status");
+
+        foreach (Quest quest in questStack.quests)
+        {
+            if (quest.IsCompleted ())
+            {
+                /// TODO
+            }
+            else
+            {
+                /// TODO
+            }
+        }
     }
 
-    public void SetQuestStorage(QuestStack newQuestStorage )
+    public void SetQuestStorage(QuestStack newQuestStorage)
     {
         QuestStorage = newQuestStorage;
+        QuestStorage.OnChange += ChangeQuestsUI;
     }
 }
