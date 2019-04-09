21b8dc81ca8546745ab79730c361f00e4beeff19
Martin Drozdík <drozdik.m@seznam.cz>
2019-04-09 21:54:44 +0200

    Sync

---
+++ Unity/Assets/Scripts/StageManager/StageTypes/StageInteracted.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Stage that awaits interaction
+/// </summary>
+public class StageInteracted : Stage
+{
+    private bool hasBeenInteractedWith = false;
+
+
+
+    public override bool ReadyForNextStage()
+    {
+        return hasBeenInteractedWith;
+    }
+
+    /// <summary>
+    /// Sets that an item has been interacted with
+    /// </summary>
+    public void HasBeenInteractedWith()
+    {
+        hasBeenInteractedWith = true;
+    }
+
+    public override void StageEnd()
+    {
+        
+    }
+
+    public override void StageFixedUpdate()
+    {
+        
+    }
+
+    public override void StageLoad()
+    {
+        
+    }
+
+    public override void StageUpdate()
+    {
+       
+    }
+
+
+}
