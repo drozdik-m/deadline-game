89a62482aa08c426f54106674f1587a124775b06
Lukáš Pekař <lukaspekar8@gmail.com>
2019-04-10 22:30:41 +0200

    add dialog reaction implementation

--- Unity/Assets/Scripts/Interaction/Reactions/ImmediateReactions/DialogReaction.cs
+++ Unity/Assets/Scripts/Interaction/Reactions/ImmediateReactions/DialogReaction.cs
@@ -8,10 +8,37 @@ using UnityEngine;
 public class DialogReaction : Reaction
 {
     // sem si lze pridat jakekoliv public atributy potrebne k zavolani dialogu
+    private GameObject dialogManagerObj;
+    private DialogManager dialogManager;
+
+    public TwinTalkDialog twinTalkDialog;
+    public SelfTalkDialog selfTalkDialog;
+
+    public GameObject target;
+
+    protected override void SpecificInit()
+    {
+        dialogManagerObj = GameObject.FindGameObjectWithTag("DialogManager");
+        if (dialogManagerObj == null)
+        {
+            Debug.LogError("Dialog Manager was not found -> you need to add it to the scene");
+            return;
+        }
+
+        dialogManager = dialogManagerObj.GetComponent<DialogManager>();
+        if (dialogManager == null)
+        {
+            Debug.LogError("Dialog Manager Object does not have Dialog Manager Compoment -> you need to add it");
+            return;
+        }
+    }
 
     protected override void ImmediateReaction()
     {
-        // tento kod probehne pri interakci
-        Debug.Log("dialog reaction -> not implemented");
+        if (twinTalkDialog != null)
+            dialogManager.AddDialog(twinTalkDialog, target);
+        else
+            dialogManager.AddDialog(selfTalkDialog);
     }
+
 }
