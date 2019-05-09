2ff048b931c590ef7312b480d642766121862641
Martin Drozdík <drozdik.m@seznam.cz>
2019-05-09 21:29:05 +0200

    Add stage fade editor

--- Unity/Assets/Editor/StageManagerEditor/Stages/StageFadeEditor.cs
+++ Unity/Assets/Editor/StageManagerEditor/Stages/StageFadeEditor.cs
@@ -13,6 +13,20 @@ public class StageFadeEditor : StageEditor
 
     public override void OnStageInspectorGUI()
     {
-        DrawDefaultInspector();
+        //DrawDefaultInspector();
+
+        StageFade Target = base.Target as StageFade;
+
+        //Fade color
+        Target.FadeColor = EditorGUILayout.ColorField("Fading color", Target.FadeColor);
+
+        //Fade length
+        Target.FadeLength = EditorGUILayout.FloatField("Fade length [sec]", Target.FadeLength);
+        if (Target.FadeLength < 0)
+            MessageBox.AddMessage("Fade length is negative", ErrorStyle);
+
+        //Fade style
+        Target.FadeStyle = (StageFadeOption)EditorGUILayout.EnumPopup("Fading style", Target.FadeStyle);
+
     }
 }
