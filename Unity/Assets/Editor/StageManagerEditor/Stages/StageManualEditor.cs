03dea2767c6dc52daff64e0b8023cac60528002f
Martin Drozdík <drozdik.m@seznam.cz>
2019-05-12 18:28:39 +0200

    Add manual stage

---
+++ Unity/Assets/Editor/StageManagerEditor/Stages/StageManualEditor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(StageManual))]
+public class StageManualEditor : StageEditor
+{
+    public override string GetFoldoutLabel()
+    {
+        return "StageManual";
+    }
+
+    public override void OnStageInspectorGUI()
+    {
+        StageManual Target = base.Target as StageManual;
+
+        //Move
+        Target.MoveToNextStage = EditorGUILayout.Toggle("Move to next stage", Target.MoveToNextStage);
+    }
+}
