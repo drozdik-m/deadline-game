536c8655747b041803ea568d7a8047799387b520
Ian Mustiats <mustiian@fit.cvut.cz>
2019-07-21 17:33:17 +0200

    Add new font. Create script for UI of readable objects

---
+++ Unity/Assets/Scripts/ReadUI/ReadableObjectUI.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ReadableObjectUI : MonoBehaviour
+{
+    public void Open()
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            transform.GetChild(i).gameObject.SetActive(true);
+        }
+    }
+
+    public void Close()
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            transform.GetChild(i).gameObject.SetActive(false);
+        }
+    }
+}
