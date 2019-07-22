2e4f90280c1903a060744bbc029166cc3b4563f2
Ian Mustiats <mustiian@fit.cvut.cz>
2019-07-22 20:06:55 +0200

    Add continue text,  update UI and script

--- Unity/Assets/Scripts/ReadUI/ReadableObjectUI.cs
+++ Unity/Assets/Scripts/ReadUI/ReadableObjectUI.cs
@@ -5,19 +5,34 @@ using UnityEngine.UI;
 
 public class ReadableObjectUI : MonoBehaviour
 {
+    public Text ContinueText;
+
+    private float _delay;
+    private float _fadingDuration;
+    private float _alphaColor;
+
     private void Start()
     {
         SetActive(false);
+        _delay = 4.0f;
+        _fadingDuration = 2.0f;
+        _alphaColor = 0.5f;
     }
 
     public void Open()
     {
+        Color color = ContinueText.color;
+        color.a = 0;
+        ContinueText.color = color;
+
         SetActive(true);
+        StartCoroutine(ShowTextEnumerator(_delay, _fadingDuration));
     }
 
     public void Close()
     {
         SetActive(false);
+        StopAllCoroutines();
     }
 
     private void SetActive(bool status)
@@ -25,6 +40,30 @@ public class ReadableObjectUI : MonoBehaviour
         for (var i = 0; i < transform.childCount; i++)
         {
             transform.GetChild(i).gameObject.SetActive(status);
+        }
+    }
+
+    private IEnumerator ShowTextEnumerator(float duration, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+
+        // Original color of text
+        var color = ContinueText.color;
+        // Time, when the method was run
+        var startTime = Time.time;
+        // Starting text alpha
+        float alpha = 0;
+        // Fading step (depends on fading duration)
+        float step = 1;
+
+        // Chages alpha channel every frame by step
+        while (startTime + duration > Time.time)
+        {
+            step -= (1 / duration) * Time.deltaTime;
+            color.a = Mathf.Lerp(_alphaColor, alpha, step);
+            ContinueText.color = color;
+
+            yield return null;
         }
     }
 }
