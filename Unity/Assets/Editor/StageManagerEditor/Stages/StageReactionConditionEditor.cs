4b3630eb8bbfbaf45ad48cc76bc4af13de402abb
Martin Drozdík <drozdik.m@seznam.cz>
2019-07-28 12:43:59 +0200

    Stage reaction condition

---
+++ Unity/Assets/Editor/StageManagerEditor/Stages/StageReactionConditionEditor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(StageReactionCondition))]
+public class StageReactionConditionEditor : StageEditor
+{
+    public override string GetFoldoutLabel()
+    {
+        return "StageReactionCondition";
+    }
+
+    public override void OnStageInspectorGUI()
+    {
+        DrawDefaultInspector();
+    }
+}
