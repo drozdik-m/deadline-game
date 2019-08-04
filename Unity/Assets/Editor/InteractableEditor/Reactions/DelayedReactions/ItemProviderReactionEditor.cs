09635ae5a5644446e2bad6b2722111578c28a75d
Lukáš Pekař <lukaspekar8@gmail.com>
2019-08-04 11:55:23 +0200

    add item provider reaction add item provider reaction editor

---
+++ Unity/Assets/Editor/InteractableEditor/Reactions/DelayedReactions/ItemProviderReactionEditor.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+/// <summary>
+/// Custom Editor for Item Provider Reaction
+/// </summary>
+[CustomEditor(typeof(ItemProviderReaction))]
+public class ItemProviderReactionEditor : ReactionEditor
+{
+    public override string GetFoldoutLabel()
+    {
+        return "Item Provider Reaction";
+    }
+
+    public override void OnCustomInspectorGUI()
+    {
+        ItemProviderReaction thisReaction = Target as ItemProviderReaction;
+
+        thisReaction.delay = EditorGUILayout.Slider("Delay", thisReaction.delay, 0, 5);
+
+        thisReaction.itemProvider = (ItemProvider)EditorGUILayout
+            .ObjectField("Item Provider",
+                         thisReaction.itemProvider,
+                         typeof(ItemProvider),
+                         true);
+
+        if (thisReaction.itemProvider == null)
+            MessageBox.AddMessage("Item Provider is empty", WarningStyle);
+    }
+}
