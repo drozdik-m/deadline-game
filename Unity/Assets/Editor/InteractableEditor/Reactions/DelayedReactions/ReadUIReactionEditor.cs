d64e4bb424a8ecf0e6d3ba65df35e535a4542fc1
Lukáš Pekař <lukaspekar8@gmail.com>
2019-08-04 13:12:08 +0200

    add ReadUIReaction add ReadUIReactionEditor

---
+++ Unity/Assets/Editor/InteractableEditor/Reactions/DelayedReactions/ReadUIReactionEditor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(ReadUIReaction))]
+public class ReadUIReactionEditor : ReactionEditor
+{
+    public override string GetFoldoutLabel()
+    {
+        return "Read UI Reaction";
+    }
+
+    public override void OnCustomInspectorGUI()
+    {
+        ReadUIReaction thisReaction = Target as ReadUIReaction;
+
+        thisReaction.readableObjectUI = (ReadableObjectUI)EditorGUILayout
+            .ObjectField("Readable Object UI",
+                         thisReaction.readableObjectUI,
+                         typeof(ReadableObjectUI),
+                         true);
+
+        if (thisReaction.readableObjectUI == null)
+            MessageBox.AddMessage("Readable Object UI is empty", WarningStyle);
+    }
+}
