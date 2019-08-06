27b01b7a92e0d2ae176831ccba584775baa863eb
Ian Mustiats <mustiian@fit.cvut.cz>
2019-08-06 20:44:04 +0200

    Create a schematic structure of the Tab Menu, add script to managing tab menu

---
+++ Unity/Assets/Scripts/TabMenu/MenuTabsManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuTabsManager : MonoBehaviour
+{
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void Open(GameObject tab)
+    {
+
+    }
+
+    public void Close(GameObject tab)
+    {
+
+    }
+}
