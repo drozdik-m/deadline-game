705ee0a985d600aa41607c75589f18a95e087684
Ian Mustiats <mustiian@fit.cvut.cz>
2019-08-11 15:05:43 +0200

    Add general methods and UI canvas

--- Unity/Assets/Scripts/Transformer/UITransformer/TransformerUIController.cs
+++ Unity/Assets/Scripts/Transformer/UITransformer/TransformerUIController.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class TransformerUIController : MonoBehaviour
 {
+    public GameObject Transformer;
+
+    /// <summary>
+    /// Contain all the images available using the type of the item
+    /// </summary>
+    private Dictionary<InventoryItemID, Sprite> spritesStorage;
+
+    void Awake()
+    {
+        var imagesStorage = GameObject.FindGameObjectWithTag("MasterUI").GetComponent<UIMaster>().InventoryUI.ImagesStorage;
+
+        spritesStorage = new Dictionary<InventoryItemID, Sprite>();
+
+        foreach (InventoryUIController.ItemImage image in imagesStorage)
+        {
+            spritesStorage.Add(image.type, image.sprite);
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OpenUIDialog()
     {
-        
+
+    }
+
+    public void CloseUIDialog()
+    {
+
+    }
+
+    public void UpdateState()
+    {
+
     }
 }
