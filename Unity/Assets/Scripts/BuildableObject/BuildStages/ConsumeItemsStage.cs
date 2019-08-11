67fd53bbb27e657cb8660aa0da4eb9094808eed2
Martin Drozdík <drozdik.m@seznam.cz>
2019-08-11 19:52:04 +0200

    Add prototype stages

---
+++ Unity/Assets/Scripts/BuildableObject/BuildStages/ConsumeItemsStage.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+/// <summary>
+/// Build Stage that needs item in inventory to be done and after commiting to stage, the item will be removed
+/// </summary>
+public class ConsumeItemsStage : BuildStage
+{
+    /// <summary>
+    /// Optional inventory to check item in
+    /// </summary>
+    public Inventory overrideInventory;
+
+    /// <summary>
+    /// Desired items and their count needed
+    /// </summary>
+    public Dictionary<InventoryItemID, int> RequiredItems;
+
+    /// <summary>
+    /// Checks if the conditions for going to next stage are satisfied
+    /// </summary>
+    /// <returns></returns>
+    public override bool ConditionsSatisfied()
+    {
+        // check if the item is in the inventory
+        if (overrideInventory == null)
+            overrideInventory = GameObject.FindGameObjectWithTag("MainInventory").GetComponent<Inventory>();
+
+        //Check null inventory
+        if (overrideInventory == null)
+        {
+            Debug.LogError("PersistentItemStage: Inventory is null event after trying to find it");
+            return false;
+        }
+
+        //Try to take the item
+        var currentItem = overrideInventory.CurrentItem.ItemType;
+        var countNeeded = RequiredItems[currentItem]; //Exception?
+
+        //Decrement 
+        if (countNeeded > 0)
+        {
+            DestroyInventoryItem();
+            RequiredItems[currentItem] = countNeeded - 1;
+        }
+
+        return RequiredItems.All(e => e.Value <= 0);
+    }
+
+    /// <summary>
+    /// Destroys item in the inventory
+    /// </summary>
+    private void DestroyInventoryItem()
+    {
+        overrideInventory.DisposeCurrentItem();
+    }
+}
---
+++ Unity/Assets/Scripts/BuildableObject/BuildStages/WaitAndGive.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Represents build stage for what player does not need anything
+/// </summary>
+public class WaitAndGive : BuildStage
+{
+
+    public bool CounterFinished { get; set; } = false;
+
+    public override bool ConditionsSatisfied()
+    {
+        if (CounterFinished)
+        {
+            //Provide set item
+
+            return true;
+        }
+
+        return false;
+    }
+
+    public override void Init()
+    {
+        base.Init();
+        StartCounter();
+    }
+
+    public void StartCounter()
+    {
+        //Set CounterFinished to true after time [ms]
+    }
+}
