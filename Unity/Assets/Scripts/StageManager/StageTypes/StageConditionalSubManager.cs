f8fc76d4344394c8ca19e0e23f0ee396d93707aa
Martin Drozdík <drozdik.m@seznam.cz>
2019-09-03 17:24:13 +0200

    Add StageConditionalSubManager

---
+++ Unity/Assets/Scripts/StageManager/StageTypes/StageConditionalSubManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Stage that runs sub stage manager
+/// </summary>
+public class StageConditionalSubManager : StageSubManager
+{
+    /// <summary>
+    /// Condition to check
+    /// </summary>
+    public Condition Condition = null;
+
+    public override void StageLoad()
+    {
+        if (Condition != null && Condition.satisfied)
+        {
+            if (StageManager == null)
+                StageManager = GetComponent<StageManager>();
+            StageManager.InitiateStages();
+        }
+    }
+}
