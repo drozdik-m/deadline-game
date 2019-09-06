4d4284c58249bc775fbc1808e7190be52b3d991f
Ian Mustiats <mustiian@fit.cvut.cz>
2019-09-06 19:13:06 +0200

    Fix bug with persistent stage

--- Unity/Assets/Scripts/BuildableObject/BuildableUI/PersistentItemUI.cs
+++ Unity/Assets/Scripts/BuildableObject/BuildableUI/PersistentItemUI.cs
@@ -1,8 +1,82 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class PersistentItemUI : ConsumeItemUI
+/// <summary>
+/// Class represents UI for persistent item stage
+/// </summary>
+public class PersistentItemUI : BuildableObjectUI
 {
+    /// <summary>
+    /// Background for items images, that changes dynamically
+    /// </summary>
+    private RectTransform backgroundPanel;
 
+    /// <summary>
+    /// Consume Item stage component
+    /// </summary>
+    private PersistentItemStage consumeItemStageComponent;
+
+    /// <summary>
+    /// Sprite of the desired item
+    /// </summary>
+    private Sprite desiredItemSprite;
+
+    /// <summary>
+    /// Sets necessary values for stage UI
+    /// </summary>
+    /// <param name="buildableObject">Game Object that contains Buildable object</param>
+    /// <param name="state">State text UI</param>
+    /// <param name="backgroundImagesPanel">Background for Items images</param>
+    public void SetUI(GameObject buildableObject, Text state, RectTransform backgroundImagesPanel)
+    {
+        // Set all required items with base
+        base.SetUI(buildableObject, state);
+
+        consumeItemStageComponent = buildableGameObject.GetComponentInChildren<PersistentItemStage>();
+        backgroundPanel = backgroundImagesPanel;
+
+        foreach (var image in GameObject.FindGameObjectWithTag("MasterUI").GetComponent<UIMaster>().InventoryUI.ImagesStorage)
+        {
+            if (consumeItemStageComponent.desiredItem == image.type)
+            {
+                desiredItemSprite = image.sprite;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Activates stage UI
+    /// </summary>
+    public override void Activate()
+    {
+        backgroundPanel.gameObject.SetActive(true);
+        UpdateStateText("Show");
+        CreateNewDesiredItem();
+    }
+
+    /// <summary>
+    /// Deactivates stage UI
+    /// </summary>
+    public override void Deactivate()
+    {
+        backgroundPanel.gameObject.SetActive(false);
+    }
+
+    /// <summary>
+    /// Creates and sets new Image for desired Item
+    /// </summary>
+    private void CreateNewDesiredItem()
+    {
+        Image tmpImage;
+
+        // Create prefab Image for item image
+        GameObject prefabObject = new GameObject();
+        Image imagePrefab = prefabObject.AddComponent<Image>();
+
+        imagePrefab.sprite = desiredItemSprite;
+        tmpImage = GameObject.Instantiate<Image>(imagePrefab, transform.position, transform.rotation, transform);
+        tmpImage.name = consumeItemStageComponent.desiredItem.ToString();
+    }
 }
