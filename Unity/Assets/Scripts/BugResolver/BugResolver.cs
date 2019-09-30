e9da14fe5fd13abee2fe7531666fdafda2f45d8b
Adam Gelatka <gelatada@fit.cvut.cz>
2019-09-30 22:34:19 +0200

    Add falling items bug resolver

---
+++ Unity/Assets/Scripts/BugResolver/BugResolver.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public delegate void BugResolverHandler(BugResolver source, BugResolverArgs buggedItem);
+
+public class BugResolver : MonoBehaviour
+{
+    /// <summary>
+    /// The void treshold (When is an item considered to be falling).
+    /// </summary>
+    public double voidTreshold;
+    /// <summary>
+    /// The drop point of the bugged item.
+    /// </summary>
+    private Transform dropPoint;
+    /// <summary>
+    /// Occurs when an item gets bugged (falls into void).
+    /// </summary>
+    public event BugResolverHandler OnItemBugged;
+    /// <summary>
+    /// Occurs when player gets bugged.
+    /// </summary>
+    public event BugResolverHandler OnPlayerBugged;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var mainInventory = GameObject.FindGameObjectWithTag("MainInventory");
+        if (!dropPoint)
+        {
+            dropPoint = mainInventory.transform.GetChild(0);
+        }
+        else
+        {
+            Debug.LogError("BugResolver: MainInvetory not found");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var interactableGameObjects = GameObject.FindGameObjectsWithTag("Hazardous");
+
+        foreach (var go in interactableGameObjects)
+        {
+           if (go.transform.position.y < voidTreshold)
+            {
+                var rb = go.GetComponent<Rigidbody>();
+                rb.velocity = new Vector3(0, 0, 0);
+                go.transform.position = dropPoint.position;
+                ItemBugged(go);
+
+            }
+        }
+
+    }
+    protected virtual void ItemBugged(GameObject buggedGameObject)
+    {
+        OnItemBugged?.Invoke(this, new BugResolverArgs(buggedGameObject, false));
+    }
+
+    protected virtual void PlayerBugged()
+    {
+        OnItemBugged?.Invoke(this, new BugResolverArgs(null, true));
+    }
+}
---
+++ Unity/Assets/Scripts/BugResolver/BugResolverArgs.cs
@@ -0,0 +1,49 @@
+﻿
+using UnityEngine;
+/// <summary>
+/// Bug resolver arguments.
+/// </summary>
+public class BugResolverArgs
+{
+    /// <summary>
+    /// Gets the bugged item game object.
+    /// </summary>
+    /// <value>The bugged item game object.</value>
+    public GameObject BuggedItemGameObject
+    {
+        get
+        {
+            return buggedItemGameObject;
+        }
+    }
+    /// <summary>
+    /// Gets a value indicating whether this <see cref="T:BugResolverArgs"/> player was bugged.
+    /// </summary>
+    /// <value><c>true</c> if player was bugged; otherwise, <c>false</c>.</value>
+    public bool PlayerWasBugged
+    {
+        get
+        {
+            return playerWasBugged;
+        }
+    }
+    /// <summary>
+    /// The bugged item game object.
+    /// </summary>
+    private readonly GameObject buggedItemGameObject;
+    /// <summary>
+    /// True, if the player got bugged.
+    /// </summary>
+    private readonly bool playerWasBugged;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="T:BugResolverArgs"/> class.
+    /// </summary>
+    /// <param name="buggedItemGameObject">Bugged item game object.</param>
+    /// <param name="playerWasBugged">If set to <c>true</c> player was bugged.</param>
+    public BugResolverArgs(GameObject buggedItemGameObject, bool playerWasBugged )
+    {
+        this.buggedItemGameObject = buggedItemGameObject;
+        this.playerWasBugged = playerWasBugged;
+    }
+}
