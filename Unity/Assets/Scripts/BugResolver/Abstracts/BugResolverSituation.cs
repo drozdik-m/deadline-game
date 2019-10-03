20fd1e060c66eb8c4173a94111c1ad5a49f31fac
Adam Gelatka <gelatada@fit.cvut.cz>
2019-10-03 13:48:35 +0200

    Add updated version of bug resolver

---
+++ Unity/Assets/Scripts/BugResolver/Abstracts/BugResolverSituation.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public delegate void BugResolverHandler(BugResolverSituation source, BugResolverArgs buggedObject);
+/// <summary>
+/// <see langword="abstract"/> Bug resolver situation, abstra
+/// </summary>
+public abstract class BugResolverSituation : MonoBehaviour
+{
+    /// <summary>
+    /// Protect this object
+    /// </summary>
+    /// <returns>The protect.</returns>
+    public abstract bool Protect();
+
+}
