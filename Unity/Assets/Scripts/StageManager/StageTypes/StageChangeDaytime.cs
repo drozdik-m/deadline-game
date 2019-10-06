fea8ae78f6b86e6db945669c11b3a3124104766c
Martin Drozdík <drozdik.m@seznam.cz>
2019-10-06 19:21:12 +0200

    Change daytime possibility, text updates

---
+++ Unity/Assets/Scripts/StageManager/StageTypes/StageChangeDaytime.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Stage that performs fading in/out
+/// </summary>
+public class StageChangeDaytime : Stage
+{
+    
+    public SkyboxController.Time TargetTime;
+
+    public override bool ReadyForNextStage()
+    {
+        return true;
+    }
+
+    public override void StageEnd()
+    {
+        
+    }
+
+    public override void StageFixedUpdate()
+    {
+        
+    }
+
+    public override void StageLoad()
+    {
+        FindObjectOfType<SkyboxController>().ChangeTime(TargetTime);
+    }
+
+    public override void StageUpdate()
+    {
+        
+    }
+}
